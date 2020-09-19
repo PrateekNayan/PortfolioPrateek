bce5027639f0a80404c280c7e62bd3428a7c7e36
Prateek Nayan <prateek.nayan123@gmail.com>
2020-09-19 03:03:29 -0500

    Updates

---
+++ precache-manifest.422e778dc68103a0971b2c16211a7fe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f6ae2194e57a80e26e3cda52ea1149",
+    "url": "/PortfolioPrateek/index.html"
+  },
+  {
+    "revision": "c8c626a3acd1b770f099",
+    "url": "/PortfolioPrateek/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a9021bb05bbc4a99eed8",
+    "url": "/PortfolioPrateek/static/js/2.48b8d7f0.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/PortfolioPrateek/static/js/2.48b8d7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c626a3acd1b770f099",
+    "url": "/PortfolioPrateek/static/js/main.a8a57ff1.chunk.js"
+  },
+  {
+    "revision": "58d848bcd2bc3a98c7ee",
+    "url": "/PortfolioPrateek/static/js/runtime-main.9859ce8c.js"
+  }
+]);
